fa80dc9449d28733014fe117d9d61760f524dd6f
Diogo Sousa <damsa1@iscte-iul.pt>
2025-10-02 15:28:14 +0200

    Retry workflow setup again

--- src/main/java/com/vaadin/starter/bakery/backend/service/UserService.java
+++ src/main/java/com/vaadin/starter/bakery/backend/service/UserService.java
@@ -24,6 +24,7 @@ public class UserService implements FilterableCrudService<User> {
 	/** Mensagens de erro. */
 	public static final String MODIFY_LOCKED_USER_NOT_PERMITTED = "User has been locked and cannot be modified or deleted";
 	private static final String DELETING_SELF_NOT_PERMITTED = "You cannot delete your own account";
+	
 	private final UserRepository userRepository;
 
 	/**
